1c319962f3cb476fcbd436d2acc5b561b8a53f31
emesonmikael <114108397+emesonmikael@users.noreply.github.com>
2024-09-08 17:44:55 +0000

    new file:   public/Globoplaylista.m3u 	modified:   src/App.js 	new file:   src/Globoplay.js 	modified:   src/Home.js 	deleted:    src/Home2.js 	new file:   src/Home3.js

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import M3UPlayer from './M3UPlayer';
 import PlayerPage from './PlayerPage';
 import Home from './Home';
 import M3UPlayerHbo from './M3UPlayer copy';
+import M3UPlayerGloboplay from './Globoplay';
 
 const App = () => {
   return (
@@ -11,6 +12,7 @@ const App = () => {
       <Routes>
       <Route path="/" element={<Home />} />
         <Route path="/netfli" element={<M3UPlayer />} />
+        <Route path="/Globoplay" element={<M3UPlayerGloboplay />} />
         <Route path="/Hbo" element={<M3UPlayerHbo />} />
         <Route path="/netfli/player/:channelName" element={<PlayerPage />} />
         <Route path="/Hbo/player/:channelName" element={<PlayerPage />} />
