d946d1d956fd31d104fccfec7c87ed401709cfa2
emesonmikael <114108397+emesonmikael@users.noreply.github.com>
2024-09-18 11:07:28 -0300

    subir3

--- src/PlayerPage.js
+++ src/PlayerPage.js
@@ -11,38 +11,13 @@ const PlayerPage = () => {
   const params = new URLSearchParams(location.search);
   const url = params.get('url');
   const [walletAddress, setWalletAddress] = useState(null);
-  try {
-    let provider;
-    // Configurando WalletConnect com o Infura
-
-
-    // Tenta usar MetaMask primeiro
-    if (window.ethereum) {
-      provider = new ethers.providers.Web3Provider(window.ethereum);
-      await provider.send("eth_requestAccounts", []); // Solicita ao usuário que conecte a carteira
-    } else {
-      // Se não houver MetaMask, tenta conectar com WalletConnect
-      //const YOUR_INFURA_PROJECT_ID = '4f2cf2bc50c8496bb379695691632d3d'; // Coloque o seu Project ID aqui
-     
-
-      //const instance = await web3Modal.connect();
-      //const web3Provider = new ethers.providers.Web3Provider(instance);
-     // setProvider(web3Provider);
-
-     // const signer = web3Provider.getSigner();
-     // const address = await signer.getAddress();
-      //setWalletAddress(address);
-
-     // provider = new ethers.providers.Web3Provider(await walletConnect.getProvider());
-    }
-
-    const signer = provider.getSigner();
-    const address = await signer.getAddress();
-    setWalletAddress(address); // Define o endereço da carteira conectada
-  } catch (error) {
-    console.error("Erro ao conectar a carteira:", error);
-    alert("Não foi possível conectar a carteira. Verifique se você possui uma carteira instalada.");
-  }
+  if (window.ethereum) {
+    provider = new ethers.providers.Web3Provider(window.ethereum);
+    await provider.send("eth_requestAccounts", []); // Solicita ao usuário que conecte a carteira
+  } 
+  const signer = provider.getSigner();
+  const address = await signer.getAddress();
+  setWalletAddress(address); // Define o endereço da carteira conectada
 
   return (
     <div>
