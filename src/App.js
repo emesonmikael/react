2c35d6f7b5bf730cd23102eebf169ae64c69d9fd
emesonmikael <114108397+emesonmikael@users.noreply.github.com>
2024-09-20 20:28:05 -0300

    h

--- src/App.js
+++ src/App.js
@@ -7,6 +7,25 @@ import M3UPlayer from './M3UPlayer';
 import PlayerPage from './PlayerPage';
 import Conteudo from './conteudo';
 import M3UPlayerHbo from './M3UPlayer copy';
+import M3UPlayerGloboplay from './Globoplay';
+import M3UPlayerAmazonPrime from './ amazon';
+import M3UPlayerDisneyPlus from './DisneyPlus';
+import M3UPlayerParamountPlus from './ParamountPlus';
+import M3UPlayerMax from './Max';
+import M3UPlayerClarovideo from './Clarovideo';
+import M3UPlayerGooglePlayMovies from './GooglePlayMovies';
+import M3UPlayerMGMChannel from './MGMChannel';
+import M3UPlayerOldflix from './Oldflix';
+import M3UPlayerNOW from './NOW';
+import M3UPlayerVudu from './Vudu';
+import M3UPlayerAMConDemand from './AMConDemand';
+import M3UPlayerParamountPlusAppleTVChannel from './ParamountPlusAppleTVChannel';
+import M3UPlayerUniverVideo from './UniverVideo';
+import M3UPlayerNetflixbasicwithAds from './NetflixbasicwithAds';
+import M3UPlayerOiPlay from './OiPlay';
+import M3UPlayerLooke from './Looke';
+import M3UPlayerFilmicca from './Filmicca';
+import M3UPlayerLancamentos2024 from './Lancamentos2024';
 
 function App() {
   const [account, setAccount] = useState(null);
@@ -81,8 +100,51 @@ function App() {
             }
           />
            <Route path="/Hbo" element={<M3UPlayerHbo />} />
-           <Route path="/Hbo/player/:channelName" element={<PlayerPage />} />
+           <Route path="/Globo" element={<M3UPlayerGloboplay />} />
+           <Route path="/DisneyPlus" element={<M3UPlayerDisneyPlus />} />
+          <Route path="/AmazonPrime" element={<M3UPlayerAmazonPrime />} />
+          <Route path="/ParamountPlus" element={<M3UPlayerParamountPlus />} />
+          <Route path="/Max" element={<M3UPlayerMax />} />
+          <Route path="/Clarovideo" element={<M3UPlayerClarovideo />} />
+          <Route path="/GooglePlayMovies" element={<M3UPlayerGooglePlayMovies />} />
+          <Route path="/MGMChannel" element={<M3UPlayerMGMChannel />} />
+          <Route path="/Oldflix" element={<M3UPlayerOldflix />} />
+          <Route path="/NOW" element={<M3UPlayerNOW />} />
+          <Route path="/Vudu" element={<M3UPlayerVudu />} />
+          <Route path="/AMConDemand" element={<M3UPlayerAMConDemand />} />
+          <Route path="/ParamountPlusAppleTVChannel" element={<M3UPlayerParamountPlusAppleTVChannel />} />
+          <Route path="/UniverVideo" element={<M3UPlayerUniverVideo />} />
+          <Route path="/NetflixbasicwithAds" element={<M3UPlayerNetflixbasicwithAds />} />
+          <Route path="/OiPlay" element={<M3UPlayerOiPlay />} />
+          <Route path="/Looke" element={<M3UPlayerLooke />} />
+          <Route path="/Filmicca" element={<M3UPlayerFilmicca />} />
+          <Route path="/Lancamentos2024" element={<M3UPlayerLancamentos2024 />} />
+
+           <Route path="/Hbo/player/:channelName" element={<PrivateRoute><PlayerPage /></PrivateRoute>} />
           <Route path="/netfli/player/:channelName" element={<PrivateRoute><PlayerPage /></PrivateRoute>} />
+          <Route path="/Globo/player/:channelName" element={{<PrivateRoute><PlayerPage /></PrivateRoute>} />
+        <Route path="/DisneyPlus/player/:channelName" element={{<PrivateRoute><PlayerPage /></PrivateRoute>} />
+        <Route path="/AmazonPrime/player/:channelName" element={<PlayerPage />} />
+
+        <Route path="/ParamountPlus/player/:channelName" element={<PlayerPage />} />
+        <Route path="/Max/player/:channelName" element={<PlayerPage />} />
+        <Route path="/Clarovideo/player/:channelName" element={<PlayerPage />} />
+        <Route path="/GooglePlayMovies/player/:channelName" element={<PlayerPage/>} />
+
+        <Route path="/MGMChannel/player/:channelName" element={<PlayerPage />} />
+        <Route path="/Oldflix/player/:channelName" element={<PlayerPage  />} />
+
+        <Route path="/NOW/player/:channelName" element={<PlayerPage  />} />
+        <Route path="/Vudu/player/:channelName" element={<PlayerPage />} />
+        <Route path="/AMConDemand/player/:channelName" element={<PlayerPage />} />
+        <Route path="/ParamountPlusAppleTVChannel/player/:channelName" element={<PlayerPage  />} />
+        <Route path="/UniverVideo/player/:channelName" element={<PlayerPage  />} />
+        
+        <Route path="/NetflixbasicwithAds/player/:channelName" element={<PlayerPage/>} />
+        <Route path="/OiPlay/player/:channelName" element={<PlayerPage />} />
+        <Route path="/Looke/player/:channelName" element={<PlayerPage />} />
+        <Route path="/Filmicca/player/:channelName" element={<PlayerPage />} />
+        <Route path="/Lancamentos2024/player/:channelName" element={<PlayerPage />} />
           <Route path="/login" element={<LoginPage connectWallet={connectWallet} />} />
         </Routes>
       </div>
