43418ccb315b0fdda15620353fcb4505f5355618
emesonmikael <114108397+emesonmikael@users.noreply.github.com>
2024-09-20 19:41:54 -0300

    subir

--- src/LoginPage.js
+++ src/LoginPage.js
@@ -6,7 +6,7 @@ const LoginPage = ({ connectWallet }) => {
 
   const handleLogin = async () => {
     await connectWallet();
-    navigate("/netfli"); // Redireciona para a página protegida após a conexão
+    //navigate("/netfli"); // Redireciona para a página protegida após a conexão
   };
 
   return (
