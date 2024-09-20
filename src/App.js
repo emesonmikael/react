f2a09e3db4a03c7a942de1c5eac6536d64c7aa21
emesonmikael <114108397+emesonmikael@users.noreply.github.com>
2024-09-20 18:00:06 -0300

    subir3

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Route, Routes, Navigate } from "react-router-d
 import HomePage from "./HomePage";
 import ProtectedPage from "./ProtectedPage";
 import LoginPage from "./LoginPage";
+import M3UPlayer from './M3UPlayer';
 
 function App() {
   const [account, setAccount] = useState(null);
@@ -66,6 +67,8 @@ function App() {
               </PrivateRoute>
             }
           />
+           <Route path="/netfli" element={<PrivateRoute><M3UPlayer /> </PrivateRoute>} />
+           <Route path="/netfli/player/:channelName" element={<PrivateRoute><PlayerPage /></PrivateRoute>} />
           <Route path="/login" element={<LoginPage connectWallet={connectWallet} />} />
         </Routes>
       </div>
--- src/ProtectedPage.js
+++ src/ProtectedPage.js
@@ -6,6 +6,12 @@ const ProtectedPage = () => {
     <div>
       <h2>Página Protegida</h2>
       <p>Você só pode ver isso porque está conectado com sua carteira.</p>
+      <a href="https://streamer-liard.vercel.app/netfli">
+      Netflix Filme  
+<p>
+
+</p>
+</a>
     </div>
   );
 };
