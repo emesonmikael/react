82d2e0ffbe2372333bbcf40036655f0efab0d2a8
emesonmikael <114108397+emesonmikael@users.noreply.github.com>
2024-09-20 19:32:56 -0300

    subir

--- src/App.js
+++ src/App.js
@@ -78,6 +78,7 @@ function App() {
               </PrivateRoute>
             }
           />
+          <Route path="/netfli/player/:channelName" element={<PlayerPage />} />
           <Route path="/login" element={<LoginPage connectWallet={connectWallet} />} />
         </Routes>
       </div>
