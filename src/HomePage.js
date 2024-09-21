bb62e2ac3c45729b54948d9f071f9c4a1c439d80
emesonmikael <114108397+emesonmikael@users.noreply.github.com>
2024-09-20 21:21:35 -0300

    s

--- src/HomePage.js
+++ src/HomePage.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
+import { ethers } from 'ethers';
 import SubscriberManagerABI from './SubscriberManagerABI.json'; // ABI do contrato
 
 const HomePage = () => {
