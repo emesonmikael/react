b8b7e7623989fc817fd3da0036f2028e5e0ea7c4
emesonmikael <114108397+emesonmikael@users.noreply.github.com>
2024-09-21 14:08:32 -0300

    fh

--- src/LoginPage.js
+++ src/LoginPage.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
 import { ethers } from 'ethers';
+import SubscriberManagerABI from './SubscriberManagerABI.json'; // ABI do contrato
 
 
 const LoginPage = ({ connectWallet }) => {
