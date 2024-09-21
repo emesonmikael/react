ff71da7a289e1d908fd82d665af377599aa277f5
emesonmikael <114108397+emesonmikael@users.noreply.github.com>
2024-09-21 16:12:06 -0300

    n

--- src/LoginPage.js
+++ src/LoginPage.js
@@ -10,49 +10,25 @@ const LoginPage = ({ connectWallet }) => {
 
   const handleLogin = async () => {
     //await connectWallet();
-   // const savedAccount = localStorage.getItem("walletAddress");
-   // if (savedAccount) {
-     // const contractAddress = '0x2EF17eE49CC5205A2B6f3672dABEbadEDDCcDeD5';
-      //const provider = new ethers.providers.Web3Provider(window.ethereum);
-      //const signer = provider.getSigner();
-       // Conecta-se ao contrato usando a ABI e o endereço
-      // const contract = new ethers.Contract(contractAddress, SubscriberManagerABI, signer);
-       // Chama a função isSubscriberActive do contrato
-      // const status = await contract.isSubscriberActive(savedAccount);
-      // console.log(status);
-      // if(status == true){
-      //  navigate("/Conteudo"); // Redireciona para a página protegida após a conexão
-      // }
-      // else 
-     //  {alert('regitre para ter acesso aos onteudos');}
-   // }
-   if (window.ethereum) {
-    try {
-      const accounts = await window.ethereum.request({
-        method: "eth_requestAccounts",
-      });
-      setAccount(accounts[0]); // Guardar a conta conectada
-      localStorage.setItem("walletAddress", accounts[0]); // Salvar a conta no localStorage
-    } catch (err) {
-      console.error("Erro ao conectar a carteira", err);
-    }
-  } else {
-    console.error("MetaMask não encontrada");
-  }
-    
-    const contractAddress = '0x2EF17eE49CC5205A2B6f3672dABEbadEDDCcDeD5';
+    const savedAccount = localStorage.getItem("walletAddress");
+    if (savedAccount) {
+      const contractAddress = '0x2EF17eE49CC5205A2B6f3672dABEbadEDDCcDeD5';
       const provider = new ethers.providers.Web3Provider(window.ethereum);
       const signer = provider.getSigner();
-       // Conecta-se ao contrato usando a ABI e o endereço
-       const contract = new ethers.Contract(contractAddress, SubscriberManagerABI, signer);
+      //Conecta-se ao contrato usando a ABI e o endereço
+      const contract = new ethers.Contract(contractAddress, SubscriberManagerABI, signer);
        // Chama a função isSubscriberActive do contrato
-       const status = await contract.isSubscriberActive();
+       const status = await contract.isSubscriberActive(savedAccount);
        console.log(status);
        if(status == true){
         navigate("/Conteudo"); // Redireciona para a página protegida após a conexão
        }
        else 
-       {alert('regitre para ter acesso aos onteudos');}
+      {alert('regitre para ter acesso aos onteudos');}
+    }
+  
+    
+   
     
    
   };
