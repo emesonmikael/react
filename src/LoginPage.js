c7cb529199e7c7a31806616308dea0b9375f187e
emesonmikael <114108397+emesonmikael@users.noreply.github.com>
2024-09-21 14:04:31 -0300

    h

--- src/LoginPage.js
+++ src/LoginPage.js
@@ -11,8 +11,8 @@ const LoginPage = ({ connectWallet }) => {
     await connectWallet();
     const savedAccount = localStorage.getItem("walletAddress");
     if (savedAccount) {
-      const contractAddress = '0x2EF17eE49CC5205A2B6f3672dABEbadEDDCcDeD5
-      //const provider = new ethers.providers.Web3Provider(window.ethereum);';
+      const contractAddress = '0x2EF17eE49CC5205A2B6f3672dABEbadEDDCcDeD5';
+      //const provider = new ethers.providers.Web3Provider(window.ethereum);
     }
     await connectWallet();
     navigate("/Conteudo"); // Redireciona para a página protegida após a conexão
