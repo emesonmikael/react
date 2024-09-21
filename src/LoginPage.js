915c2b75cf1ffdd5d1a65aab1d4c44f7f709daef
emesonmikael <114108397+emesonmikael@users.noreply.github.com>
2024-09-21 13:41:01 -0300

    n

--- src/LoginPage.js
+++ src/LoginPage.js
@@ -1,16 +1,16 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
-import React, { useState, useEffect } from "react";
+
 
 const LoginPage = ({ connectWallet }) => {
   const navigate = useNavigate();
-  const [account, setAccount] = useState(null);
+  
 
   const handleLogin = async () => {
     await connectWallet();
     const savedAccount = localStorage.getItem("walletAddress");
     if (savedAccount) {
-      setAccount(savedAccount);
+      
     }
     navigate("/Conteudo"); // Redireciona para a página protegida após a conexão
   };
