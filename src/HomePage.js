14fb42b365a76b2954cdfc9b1297ddcaed366743
emesonmikael <114108397+emesonmikael@users.noreply.github.com>
2024-09-20 21:32:51 -0300

    s

--- src/HomePage.js
+++ src/HomePage.js
@@ -6,6 +6,18 @@ import  { useState, useEffect } from 'react';
 
 const HomePage = () => {
   const navigate = useNavigate();
+  const [referralLink, setReferralLink] = useState('');
+    const [account, setAccount] = useState('');
+    const [referrer, setReferrer] = useState('');
+    const [subscriberInfo, setSubscriberInfo] = useState(null);
+    const [planId, setPlanId] = useState('');
+    const [success, setSuccess] = useState(null);
+    const [errorMessage, setErrorMessage] = useState('');
+    const [planPrice, setPlanPrice] = useState(null);
+
+    const contractAddress = '0x823B305461153DEaa4B5f0dE85C0310ff1c235C6'; // Endereço do contrato
+    const paymentTokenAddress = '0x7D928bDC1Ae6dCC6b4D7c744c3603aD4f64e874f'; // Endereço do token de pagamento
+
 
   const goToLogin = () => {
     navigate("/login"); // Redireciona para a página de login
