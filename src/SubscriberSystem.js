b78e7ad8041c16402a3673cd20cd80eb0001e2d2
emesonmikael <114108397+emesonmikael@users.noreply.github.com>
2024-09-20 21:27:38 -0300

    e

--- src/SubscriberSystem.js
+++ src/SubscriberSystem.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import  { useState, useEffect } from 'react';
 import { ethers } from 'ethers';
 import SubscriberManagerABI from './SubscriberManagerABI.json'; // ABI do contrato
 
