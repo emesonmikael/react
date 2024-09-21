1c7bbfc215f34f051214ebc2e259d9aa45c64c65
emesonmikael <114108397+emesonmikael@users.noreply.github.com>
2024-09-21 16:09:25 -0300

    jb

--- src/LoginPage.js
+++ src/LoginPage.js
@@ -26,15 +26,27 @@ const LoginPage = ({ connectWallet }) => {
       // else 
      //  {alert('regitre para ter acesso aos onteudos');}
    // }
-    const walletAddress = await connectWallet();
-    console.log(walletAddress.toString());
+   if (window.ethereum) {
+    try {
+      const accounts = await window.ethereum.request({
+        method: "eth_requestAccounts",
+      });
+      setAccount(accounts[0]); // Guardar a conta conectada
+      localStorage.setItem("walletAddress", accounts[0]); // Salvar a conta no localStorage
+    } catch (err) {
+      console.error("Erro ao conectar a carteira", err);
+    }
+  } else {
+    console.error("MetaMask não encontrada");
+  }
+    
     const contractAddress = '0x2EF17eE49CC5205A2B6f3672dABEbadEDDCcDeD5';
       const provider = new ethers.providers.Web3Provider(window.ethereum);
       const signer = provider.getSigner();
        // Conecta-se ao contrato usando a ABI e o endereço
        const contract = new ethers.Contract(contractAddress, SubscriberManagerABI, signer);
        // Chama a função isSubscriberActive do contrato
-       const status = await contract.isSubscriberActive(walletAddress);
+       const status = await contract.isSubscriberActive();
        console.log(status);
        if(status == true){
         navigate("/Conteudo"); // Redireciona para a página protegida após a conexão
