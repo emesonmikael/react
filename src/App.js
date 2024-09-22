481dd92007d36b996e9fccf79cca344d664a6055
emesonmikael <emesonmikael@gmail.com>
2024-09-22 13:52:33 -0300

    modified:   src/App.js

--- src/App.js
+++ src/App.js
@@ -5,29 +5,8 @@ import ContentPage from './ContentPage';
 import { useState } from 'react';
 import PrivateRoute from './PrivateRoute';  // Componente de rota privada
 import Conteudo from './conteudo';
-import M3UPlayer from './M3UPlayer';
-import PlayerPage from './PlayerPage';
-import HomePage from "./Home";
-import M3UPlayerHbo from './M3UPlayer copy';
 import M3UPlayerGloboplay from './Globoplay';
-import M3UPlayerAmazonPrime from './ amazon';
-import M3UPlayerDisneyPlus from './DisneyPlus';
-import M3UPlayerParamountPlus from './ParamountPlus';
-import M3UPlayerMax from './Max';
-import M3UPlayerClarovideo from './Clarovideo';
-import M3UPlayerGooglePlayMovies from './GooglePlayMovies';
-import M3UPlayerMGMChannel from './MGMChannel';
-import M3UPlayerOldflix from './Oldflix';
-import M3UPlayerNOW from './NOW';
-import M3UPlayerVudu from './Vudu';
-import M3UPlayerAMConDemand from './AMConDemand';
-import M3UPlayerParamountPlusAppleTVChannel from './ParamountPlusAppleTVChannel';
-import M3UPlayerUniverVideo from './UniverVideo';
-import M3UPlayerNetflixbasicwithAds from './NetflixbasicwithAds';
-import M3UPlayerOiPlay from './OiPlay';
-import M3UPlayerLooke from './Looke';
-import M3UPlayerFilmicca from './Filmicca';
-import M3UPlayerLancamentos2024 from './Lancamentos2024';
+
 
 function App() {
   const [isRegistered, setIsRegistered] = useState(false);
