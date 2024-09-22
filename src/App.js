226997827fbc7d04273607131a3bbf26ad22f406
emesonmikael <emesonmikael@gmail.com>
2024-09-22 14:31:17 -0300

    subie

--- src/App.js
+++ src/App.js
@@ -1,19 +1,45 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { useState, useEffect } from 'react';
 import LoginPage from './LoginPage';
 import ContentPage from './ContentPage';
-//import AnotherPrivatePage from './AnotherPrivatePage';  // Exemplo de outra rota privada
-import { useState } from 'react';
+import M3UPlayer from './M3UPlayer';
+import PlayerPage from './PlayerPage';
 import PrivateRoute from './PrivateRoute';  // Componente de rota privada
-import Conteudo from './conteudo';
+import M3UPlayerHbo from './M3UPlayer copy';
 import M3UPlayerGloboplay from './Globoplay';
-
+import M3UPlayerAmazonPrime from './ amazon';
+import M3UPlayerDisneyPlus from './DisneyPlus';
+import M3UPlayerParamountPlus from './ParamountPlus';
+import M3UPlayerMax from './Max';
+import M3UPlayerClarovideo from './Clarovideo';
+import M3UPlayerGooglePlayMovies from './GooglePlayMovies';
+import M3UPlayerMGMChannel from './MGMChannel';
+import M3UPlayerOldflix from './Oldflix';
+import M3UPlayerNOW from './NOW';
+import M3UPlayerVudu from './Vudu';
+import M3UPlayerAMConDemand from './AMConDemand';
+import M3UPlayerParamountPlusAppleTVChannel from './ParamountPlusAppleTVChannel';
+import M3UPlayerUniverVideo from './UniverVideo';
+import M3UPlayerNetflixbasicwithAds from './NetflixbasicwithAds';
+import M3UPlayerOiPlay from './OiPlay';
+import M3UPlayerLooke from './Looke';
+import M3UPlayerFilmicca from './Filmicca';
+import M3UPlayerLancamentos2024 from './Lancamentos2024';
 
 function App() {
   const [isRegistered, setIsRegistered] = useState(false);
 
+  // Função para salvar o estado de login quando o login for bem-sucedido
   const handleLoginSuccess = () => {
     setIsRegistered(true);
+    localStorage.setItem('isRegistered', 'true');  // Salva no localStorage
   };
+
+  // Quando o aplicativo é carregado, verificar o localStorage
+  useEffect(() => {
+    const registered = localStorage.getItem('isRegistered') === 'true';
+    setIsRegistered(registered);  // Restaura o estado de login do localStorage
+  }, []);
 
   return (
     <Router>
@@ -26,14 +52,64 @@ function App() {
           path="/content"
           element={
             <PrivateRoute isRegistered={isRegistered}>
-              <Conteudo />
+              <ContentPage />
             </PrivateRoute>
           }
         />
+        <Route path="/Hbo" element={<M3UPlayerHbo />} />
 
         {/* Outra rota privada */}
-        <Route path="/Globo" element={<PrivateRoute isRegistered={isRegistered}><M3UPlayerGloboplay /></PrivateRoute>} />
+        <Route
+          path="/netfli"
+          element={
+            
+              <M3UPlayer />
+              
+          }
+        />
+        <Route path="/DisneyPlus" element={<M3UPlayerDisneyPlus />} />
+        <Route path="/AmazonPrime" element={<M3UPlayerAmazonPrime />} />
+        <Route path="/ParamountPlus" element={<M3UPlayerParamountPlus />} />
+        <Route path="/Max" element={<M3UPlayerMax />} />
+        <Route path="/Clarovideo" element={<M3UPlayerClarovideo />} />
+        <Route path="/GooglePlayMovies" element={<M3UPlayerGooglePlayMovies />} />
+        <Route path="/MGMChannel" element={<M3UPlayerMGMChannel />} />
+        <Route path="/Oldflix" element={<M3UPlayerOldflix />} />
+        <Route path="/NOW" element={<M3UPlayerNOW />} />
+        <Route path="/Vudu" element={<M3UPlayerVudu />} />
+        <Route path="/AMConDemand" element={<M3UPlayerAMConDemand />} />
+        <Route path="/ParamountPlusAppleTVChannel" element={<M3UPlayerParamountPlusAppleTVChannel />} />
+        <Route path="/UniverVideo" element={<M3UPlayerUniverVideo />} />
+        <Route path="/NetflixbasicwithAds" element={<M3UPlayerNetflixbasicwithAds />} />
+        <Route path="/OiPlay" element={<M3UPlayerOiPlay />} />
+        <Route path="/Looke" element={<M3UPlayerLooke />} />
+        <Route path="/Filmicca" element={<M3UPlayerFilmicca />} />
+        <Route path="/Lancamentos2024" element={<M3UPlayerLancamentos2024 />} />
+        <Route path="/netfli/player/:channelName" element={<PlayerPage />} />
+        <Route path="/Hbo/player/:channelName" element={<PlayerPage />} />
+        <Route path="/Globo/player/:channelName" element={<PlayerPage />} />
+        <Route path="/DisneyPlus/player/:channelName" element={<PlayerPage />} />
+        <Route path="/AmazonPrime/player/:channelName" element={<PlayerPage />} />
+
+        <Route path="/ParamountPlus/player/:channelName" element={<PlayerPage />} />
+        <Route path="/Max/player/:channelName" element={<PlayerPage />} />
+        <Route path="/Clarovideo/player/:channelName" element={<PlayerPage />} />
+        <Route path="/GooglePlayMovies/player/:channelName" element={<PlayerPage/>} />
+
+        <Route path="/MGMChannel/player/:channelName" element={<PlayerPage />} />
+        <Route path="/Oldflix/player/:channelName" element={<PlayerPage  />} />
+
+        <Route path="/NOW/player/:channelName" element={<PlayerPage  />} />
+        <Route path="/Vudu/player/:channelName" element={<PlayerPage />} />
+        <Route path="/AMConDemand/player/:channelName" element={<PlayerPage />} />
+        <Route path="/ParamountPlusAppleTVChannel/player/:channelName" element={<PlayerPage  />} />
+        <Route path="/UniverVideo/player/:channelName" element={<PlayerPage  />} />
         
+        <Route path="/NetflixbasicwithAds/player/:channelName" element={<PlayerPage/>} />
+        <Route path="/OiPlay/player/:channelName" element={<PlayerPage />} />
+        <Route path="/Looke/player/:channelName" element={<PlayerPage />} />
+        <Route path="/Filmicca/player/:channelName" element={<PlayerPage />} />
+        <Route path="/Lancamentos2024/player/:channelName" element={<PlayerPage />} />
       </Routes>
     </Router>
   );
--- src/ContentPage.js
+++ src/ContentPage.js
@@ -5,6 +5,19 @@ const ContentPage = () => {
     <div>
       <h2>Bem-vindo à página de conteúdo privado!</h2>
       <p>Somente usuários registrados têm acesso a esta página.</p>
+      <a href="http://localhost:3000/netfli">
+      Netflix Filme  
+<p>
+
+</p>
+</a>
+<p>Somente usuários registrados têm acesso a esta página.</p>
+      <a href="http://localhost:3000/Hbo">
+      Hbo 
+<p>
+
+</p>
+</a>
     </div>
   );
 };
