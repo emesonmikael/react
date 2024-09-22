aa249566c7d616c6449939b3cee556ec8e4af801
emesonmikael <114108397+emesonmikael@users.noreply.github.com>
2024-09-21 22:00:05 -0300

    hgf

--- src/LoginPage.js
+++ src/LoginPage.js
@@ -43,7 +43,7 @@ const LoginPage = ({ connectWallet }) => {
        else 
       {alert('regitre para ter acesso aos onteudos');}
     }
-    await connectWallet();
+    await connectWallet2();
   
     
    
