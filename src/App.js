f5c0f7dc580416d43b6beda1642281fdd9b4cc01
emesonmikael <emesonmikael@gmail.com>
2024-09-22 12:35:47 -0300

    modified:   src/App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
 import LoginPage from './LoginPage';
 //import PrivateRoute from './PrivateRoute';
 import ContentPage from './ContentPage';  // Página de conteúdo (rota privada)
