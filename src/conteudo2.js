d02a652c6373734683bec7b6deb375848751b15c
emesonmikael <emesonmikael@gmail.com>
2024-09-23 16:49:57 -0300

    b

--- src/conteudo2.js
+++ src/conteudo2.js
@@ -58,7 +58,7 @@ function App() {
         <Route path="/Subscriber" element={<SubscriberSystem />} />
         <Route path="/Hbo" element={<M3UPlayerHbo />} /><Route path="/Globo" element={<M3UPlayerGloboplay />} />
         <Route path="/DisneyPlus" element={<M3UPlayerDisneyPlus />} />
-        <Route path="/AmazonPrime" element={<M3UPlayerAmazonPrime />} />
+        <Route path="/AmazonPrimeVideo" element={<M3UPlayerAmazonPrime />} />
         <Route path="/ParamountPlus" element={<M3UPlayerParamountPlus />} />
         <Route path="/Max" element={<M3UPlayerMax />} />
         <Route path="/Clarovideo" element={<M3UPlayerClarovideo />} />
