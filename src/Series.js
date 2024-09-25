551f1ba4dcf86b6dbcfdea7c1dbbc5b617545533
emesonmikael <emesonmikael@gmail.com>
2024-09-24 21:03:37 -0300

    teste

--- src/Series.js
+++ src/Series.js
@@ -43,7 +43,7 @@ const parseM3U = (content) => {
 
 const App = () => {
   const [items, setItems] = useState([]);
-  const [currentUrl, setCurrentUrl] = useState(`${window.location.origin}/Series.m3u`);
+  const [currentUrl, setCurrentUrl] = useState('/Series.m3u');
 
   // Função para carregar o arquivo M3U ao iniciar
   useEffect(() => {
