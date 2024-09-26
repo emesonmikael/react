3e735f69b364713e26e5f143a1e299b7171d9797
emesonmikael <emesonmikael@gmail.com>
2024-09-26 19:44:32 -0300

    j

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css'
-import App from './App'
+//import App from './App'
 
 import reportWebVitals from './reportWebVitals';
 import conteudo2 from './conteudo2';
-//import App from './conteudo2';
+import App from './conteudo2';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
