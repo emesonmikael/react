0384e31f9e913e3e188ac5ae078773f7873bc181
emesonmikael <114108397+emesonmikael@users.noreply.github.com>
2024-09-26 08:24:44 -0300

    d

---
+++ src/App (1).js
@@ -0,0 +1,12 @@
+
+import React from 'react';
+import LiveStreamPlayer from './LiveStreamPlayer';
+
+const App = () => (
+  <div>
+    <h1>Live Stream</h1>
+    <LiveStreamPlayer />
+  </div>
+);
+
+export default App;
---
+++ src/LiveStreamPlayer.js
@@ -0,0 +1,33 @@
+
+import React, { useEffect, useRef } from "react";
+import Hls from "hls.js";
+
+const LiveStreamPlayer = () => {
+  const videoRef = useRef(null);
+
+  useEffect(() => {
+    if (videoRef.current) {
+      if (Hls.isSupported()) {
+        const hls = new Hls();
+        hls.loadSource("http://zlb177.office646851.online/auth/349.ts"); // URL do stream
+        hls.attachMedia(videoRef.current);
+        hls.on(Hls.Events.MANIFEST_PARSED, () => {
+          videoRef.current.play();
+        });
+      } else if (videoRef.current.canPlayType("application/vnd.apple.mpegurl")) {
+        videoRef.current.src = "http://zlb177.office646851.online/auth/349.ts";
+        videoRef.current.addEventListener("loadedmetadata", () => {
+          videoRef.current.play();
+        });
+      }
+    }
+  }, []);
+
+  return (
+    <div>
+      <video ref={videoRef} controls style={{ width: "100%" }} />
+    </div>
+  );
+};
+
+export default LiveStreamPlayer;
