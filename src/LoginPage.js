4f876a1cc0dafdd67d444e397735a2df73948a36
emesonmikael <emesonmikael@gmail.com>
2024-09-27 10:34:27 -0300

    jhgv

--- src/LoginPage.js
+++ src/LoginPage.js
@@ -11,7 +11,7 @@ const LoginPage = ({ onLoginSuccess }) => {
   const [error, setError] = useState('');
   
   const navigate = useNavigate();
-  const contractAddress = '0xd77814F2800FCeC938128CdbEBF844bc2cE1AB68';  // Endereço do contrato
+  const contractAddress = '0x5f16FE1A416BB62548D37F02D2ceb4305FDD37A3';  // Endereço do contrato
 
   function registrar(){
     navigate('/Subscriber?ref=0x842249350ec82e6347fee77cb29ece9131dd828b');
